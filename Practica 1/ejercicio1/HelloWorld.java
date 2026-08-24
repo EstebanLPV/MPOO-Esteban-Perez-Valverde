33abd86ec71d232daabcdd9ef3fb9921ce7a5269
esteban <esteban.valverde376@gmail.com>
2026-08-24 08:27:57 -0600

    Agrega Practica 1

---
+++ Practica 1/ejercicio1/HelloWorld.java
@@ -0,0 +1,7 @@
+package ejercicio1;
+// Luis Esteban Perez Valverde
+    public class HelloWorld {
+        public static void main(String[] args) {
+            System.out.println("Hello world");
+        }
+    }    
