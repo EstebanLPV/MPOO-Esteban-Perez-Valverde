d4bc880277314ec6c6c68fe6460fcc73e6eefc44
Esteban_LPV <149041643+EstebanLPV@users.noreply.github.com>
2026-08-27 17:51:13 -0600

    ProgramaConErrores.java

--- Practica 1/Ejercicio3/ProgramaConErrores.java
+++ Practica 1/Ejercicio3/ProgramaConErrores.java
@@ -4,8 +4,8 @@ El programa debe mostrar tres mensajes en pantalla.
 **/
 public class ProgramaConErrores {
 public static void main(String[] args) {
-System.out.println("Estoy aprendiendo Java");
-System.out.println("El compilador puede ayudarme");
-System.out.println("Voy a encontrar todos los errores.");
+  System.out.println("Estoy aprendiendo Java");
+  System.out.println("El compilador puede ayudarme");
+  System.out.println("Voy a encontrar todos los errores.");
 }
 }
